6e266f2d6f11a54eae73f7bcb86f3a65bcebe8e4
Adam Butler <Adam.J.Butler@gmail.com>
2017-11-26 19:49:04 +0000

    Update to make cinnamon changes user specific

--- manifests/config/cinnamon.pp
+++ manifests/config/cinnamon.pp
@@ -19,21 +19,16 @@ class linuxmint::config::cinnamon (
   gnome::gsettings { 'org.cinnamon_panels-enabled':
     schema => 'org.cinnamon',
     key    => 'panels-enabled',
-    value  => '[\'1:0:top\']',
+    value  => '"[\'1:0:top\']"',
+    user   => $user,
   }
 
   # Configure panel applets
   gnome::gsettings { 'org.cinnamon_enabled-applets':
     schema => 'org.cinnamon',
     key    => 'enabled-applets',
-    value  => '[\'panel1:right:0:systray@cinnamon.org:0\', \'panel1:left:0:menu@cinnamon.org:1\', \'panel1:left:1:show-desktop@cinnamon.org:2\', \'panel1:left:2:panel-launchers@cinnamon.org:3\', \'panel1:left:3:window-list@cinnamon.org:4\', \'panel1:right:1:keyboard@cinnamon.org:5\', \'panel1:right:2:notifications@cinnamon.org:6\', \'panel1:right:3:removable-drives@cinnamon.org:7\', \'panel1:right:4:user@cinnamon.org:8\', \'panel1:right:5:network@cinnamon.org:9\', \'panel1:right:7:power@cinnamon.org:11\', \'panel1:center:0:calendar@cinnamon.org:12\', \'panel1:right:9:sound@cinnamon.org:13\']',
-  }
-
-  exec { 'dbus-launch dconf reset -f /org/cinnamon/':
-    command   => '/usr/bin/dbus-launch /usr/bin/dconf reset -f /org/cinnamon/',
-    user      => $user,
-    subscribe => Gnome::Gsettings['org.cinnamon_enabled-applets'],
-    require   => Package['dconf-tools'],
+    value  => '"[\'panel1:right:0:systray@cinnamon.org:0\', \'panel1:left:0:menu@cinnamon.org:1\', \'panel1:left:1:show-desktop@cinnamon.org:2\', \'panel1:left:2:panel-launchers@cinnamon.org:3\', \'panel1:left:3:window-list@cinnamon.org:4\', \'panel1:right:1:keyboard@cinnamon.org:5\', \'panel1:right:2:notifications@cinnamon.org:6\', \'panel1:right:3:removable-drives@cinnamon.org:7\', \'panel1:right:4:user@cinnamon.org:8\', \'panel1:right:5:network@cinnamon.org:9\', \'panel1:right:7:power@cinnamon.org:11\', \'panel1:center:0:calendar@cinnamon.org:12\', \'panel1:right:9:sound@cinnamon.org:13\']"',
+    user  => $user,
   }
 
   # Configure cinnamon calendar applet
--- spec/classes/config/cinnamon_spec.rb
+++ spec/classes/config/cinnamon_spec.rb
@@ -19,22 +19,16 @@ describe 'linuxmint::config::cinnamon' do
           should contain_gnome__gsettings('org.cinnamon_panels-enabled').with(
             schema: 'org.cinnamon',
             key:    'panels-enabled',
-            value:  '[\'1:0:top\']'
+            value:  '"[\'1:0:top\']"',
+            user:   'testuser'
           )
         end
         it do
           should contain_gnome__gsettings('org.cinnamon_enabled-applets').with(
             schema: 'org.cinnamon',
             key:    'enabled-applets',
-            value:  '[\'panel1:right:0:systray@cinnamon.org:0\', \'panel1:left:0:menu@cinnamon.org:1\', \'panel1:left:1:show-desktop@cinnamon.org:2\', \'panel1:left:2:panel-launchers@cinnamon.org:3\', \'panel1:left:3:window-list@cinnamon.org:4\', \'panel1:right:1:keyboard@cinnamon.org:5\', \'panel1:right:2:notifications@cinnamon.org:6\', \'panel1:right:3:removable-drives@cinnamon.org:7\', \'panel1:right:4:user@cinnamon.org:8\', \'panel1:right:5:network@cinnamon.org:9\', \'panel1:right:7:power@cinnamon.org:11\', \'panel1:center:0:calendar@cinnamon.org:12\', \'panel1:right:9:sound@cinnamon.org:13\']'
-          )
-        end
-        it { should contain_exec('dbus-launch dconf reset -f /org/cinnamon/').that_subscribes_to('Gnome::Gsettings[org.cinnamon_enabled-applets]') }
-        it { should contain_exec('dbus-launch dconf reset -f /org/cinnamon/').that_requires('Package[dconf-tools]') }
-        it do
-          should contain_exec('dbus-launch dconf reset -f /org/cinnamon/').with(
-            command: '/usr/bin/dbus-launch /usr/bin/dconf reset -f /org/cinnamon/',
-            user:    'testuser'
+            value:  '"[\'panel1:right:0:systray@cinnamon.org:0\', \'panel1:left:0:menu@cinnamon.org:1\', \'panel1:left:1:show-desktop@cinnamon.org:2\', \'panel1:left:2:panel-launchers@cinnamon.org:3\', \'panel1:left:3:window-list@cinnamon.org:4\', \'panel1:right:1:keyboard@cinnamon.org:5\', \'panel1:right:2:notifications@cinnamon.org:6\', \'panel1:right:3:removable-drives@cinnamon.org:7\', \'panel1:right:4:user@cinnamon.org:8\', \'panel1:right:5:network@cinnamon.org:9\', \'panel1:right:7:power@cinnamon.org:11\', \'panel1:center:0:calendar@cinnamon.org:12\', \'panel1:right:9:sound@cinnamon.org:13\']"',
+            user:   'testuser'
           )
         end
         it do
