2efb4c215e910e1e22b538ca7b427d4706eda5c5
Adam Butler <Adam.J.Butler@gmail.com>
2017-12-16 10:22:46 +0000

    Refactor and simplify linuxming::config:::software_centre tests

--- spec/classes/config/software_centre_spec.rb
+++ spec/classes/config/software_centre_spec.rb
@@ -27,127 +27,41 @@ describe 'linuxmint::config::software_centre' do
             command: '/usr/bin/apt-get update'
           ).that_subscribes_to('File[/etc/apt/sources.list.d/official-package-repositories.list]')
         end
-        it do
-          should contain_gnome__gsettings('com.linuxmint.updates_show-policy-configuration').with(
-            schema: 'com.linuxmint.updates',
-            key:    'show-policy-configuration',
-            value:  false,
-            user:   'testuser'
-          )
-        end
-        it do
-          should contain_gnome__gsettings('com.linuxmint.updates_level3-is-visible').with(
-            schema: 'com.linuxmint.updates',
-            key:    'level3-is-visible',
-            value:  true,
-            user:   'testuser'
-          )
-        end
-        it do
-          should contain_gnome__gsettings('com.linuxmint.updates_level4-is-visible').with(
-            schema: 'com.linuxmint.updates',
-            key:    'level4-is-visible',
-            value:  true,
-            user:   'testuser'
-          )
-        end
-        it do
-          should contain_gnome__gsettings('com.linuxmint.updates_level3-is-safe').with(
-            schema: 'com.linuxmint.updates',
-            key:    'level3-is-safe',
-            value:  false,
-            user:   'testuser'
-          )
-        end
-        it do
-          should contain_gnome__gsettings('com.linuxmint.updates_level4-is-safe').with(
-            schema: 'com.linuxmint.updates',
-            key:    'level4-is-safe',
-            value:  false,
-            user:   'testuser'
-          )
-        end
-        it do
-          should contain_gnome__gsettings('com.linuxmint.updates_security-updates-are-safe').with(
-            schema: 'com.linuxmint.updates',
-            key:    'security-updates-are-safe',
-            value:  true,
-            user:   'testuser'
-          )
-        end
-        it do
-          should contain_gnome__gsettings('com.linuxmint.updates_kernel-updates-are-safe').with(
-            schema: 'com.linuxmint.updates',
-            key:    'kernel-updates-are-safe',
-            value:  false,
-            user:   'testuser'
-          )
-        end
-        it do
-          should contain_gnome__gsettings('com.linuxmint.updates_level1-is-visible').with(
-            schema: 'com.linuxmint.updates',
-            key:    'level1-is-visible',
-            value:  true,
-            user:   'testuser'
-          )
-        end
-        it do
-          should contain_gnome__gsettings('com.linuxmint.updates_level2-is-visible').with(
-            schema: 'com.linuxmint.updates',
-            key:    'level2-is-visible',
-            value:  true,
-            user:   'testuser'
-          )
-        end
-        it do
-          should contain_gnome__gsettings('com.linuxmint.updates_level5-is-visible').with(
-            schema: 'com.linuxmint.updates',
-            key:    'level5-is-visible',
-            value:  false,
-            user:   'testuser'
-          )
-        end
-        it do
-          should contain_gnome__gsettings('com.linuxmint.updates_level1-is-safe').with(
-            schema: 'com.linuxmint.updates',
-            key:    'level1-is-safe',
-            value:  true,
-            user:   'testuser'
-          )
-        end
-        it do
-          should contain_gnome__gsettings('com.linuxmint.updates_level2-is-safe').with(
-            schema: 'com.linuxmint.updates',
-            key:    'level2-is-safe',
-            value:  true,
-            user:   'testuser'
-          )
-        end
-        it do
-          should contain_gnome__gsettings('com.linuxmint.updates_level5-is-safe').with(
-            schema: 'com.linuxmint.updates',
-            key:    'level5-is-safe',
-            value:  false,
-            user:   'testuser'
-          )
-        end
-        it do
-          should contain_gnome__gsettings('com.linuxmint.updates_security-updates-are-visible').with(
-            schema: 'com.linuxmint.updates',
-            key:    'security-updates-are-visible',
-            value:  true,
-            user:   'testuser'
-          )
-        end
-        it do
-          should contain_gnome__gsettings('com.linuxmint.updates_kernel-updates-are-visible').with(
-            schema: 'com.linuxmint.updates',
-            key:    'kernel-updates-are-visible',
-            value:  true,
-            user:   'testuser'
-          )
+
+        schemas = {
+          'com.linuxmint.updates' => {
+            'kernel-updates-are-safe'      => false,
+            'kernel-updates-are-visible'   => true,
+            'level1-is-safe'               => true,
+            'level1-is-visible'            => true,
+            'level2-is-safe'               => true,
+            'level2-is-visible'            => true,
+            'level3-is-safe'               => false,
+            'level3-is-visible'            => true,
+            'level4-is-safe'               => false,
+            'level4-is-visible'            => true,
+            'level5-is-safe'               => false,
+            'level5-is-visible'            => false,
+            'security-updates-are-safe'    => true,
+            'security-updates-are-visible' => true,
+            'show-policy-configuration'    => false
+          }
+        }
+
+        schemas.each do |schema, settings|
+          settings.each do |key, value|
+            it do
+              should contain_gnome__gsettings("#{schema}_#{key}").with(
+                schema: schema,
+                key:    key,
+                value:  value,
+                user:   'testuser'
+              )
+            end
+          end
         end
       end
+
       context 'user param not set' do
         it do
           expect do
